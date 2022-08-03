0823bc3a36d1edf653a18b0ea4a008c0839d2627
Ebony04 <ebonytroll@gmail.com>
2022-08-03 18:23:02 +0900

    accepted one commit

--- LabSamples/LabForPractice/LabForNumericOrder/Third_Maximum_Number_414.cs
+++ LabSamples/LabForPractice/LabForNumericOrder/Third_Maximum_Number_414.cs
@@ -52,35 +52,68 @@ Constraints:
 
         public int ThirdMax(int[] nums)
         {
-            int firstInt = -232;
-            int secondInt = -232;
+            // int firstInt = -232;
+            // int secondInt = -232;
             int thirdInt = -232;
+            // firstInt = nums[0];
+            // secondInt = nums[0];
+            // thirdInt = nums[0];
 
             int tempBuffer = 0;
-            
-            // 1. check third nums
-            for (int i = 0; i < nums.Length; i++)
+            // or simply just sort and pick 3rd
+
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            nums = nums.Distinct().ToArray();
+            if(nums.Length == 1)
             {
-                tempBuffer = firstInt;
-                if(firstInt <= nums[i])
-                {
-                    firstInt = nums[i];
-                }
-                if(secondInt <= nums[i]
-                    && firstInt > nums[i])
-                {
-                    secondInt = nums[i];
-                }
-                if(thirdInt <= nums[i]
-                    && secondInt > nums[i])
-                {
-                    thirdInt = nums[i];
-                }
-
-
+                return nums[0];
             }
-            // 2. 
-            var result = 0;
+            else if(nums.Length == 2)
+            {
+                return nums[0] > nums[1] ? nums[0] : nums[1];
+            }
+            else if (nums.Length >= 3)
+            {
+                thirdInt = nums[2];
+            }
+            
+
+            #region  
+            //// 1. check third nums
+            //// [2,3,4,1,6]
+            //// 2/2/2
+            //// 3/2/2
+            //// 4/2/2
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    //if(i == 0)
+            //    //{
+            //    //    firstInt = nums[0];
+            //    //    secondInt = nums[0];
+            //    //    thirdInt = nums[0];
+            //    //}
+            //    tempBuffer = firstInt;
+            //    if(firstInt <= nums[i])
+            //    {
+            //        firstInt = nums[i];
+            //    }
+            //    if(secondInt <= nums[i]
+            //        && firstInt > nums[i])
+            //    {
+            //        secondInt = nums[i];
+            //    }
+            //    if(thirdInt <= nums[i]
+            //        && secondInt > nums[i])
+            //    {
+            //        thirdInt = nums[i];
+            //    }
+
+
+            //}
+            //// 2. 
+            //var result = 0;
+            #endregion
             return thirdInt;
         }
     }

4e56d79e1590f8b6388545923be85c69a08784d9
Sandalwood <ebonytroll@gmail.com>
2023-07-14 15:58:50 +0900

    conflict resolved by company's files

---
+++ LabSamples/LabForPractice/Fiddling/FiddlingClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabForPractice.Fiddling
+{
+    public class FiddlingClass
+    {
+        OtherFiddlingClass tempOtherClass;
+
+        public void TestShow()
+        {
+            var tempClass = tempOtherClass;
+            tempOtherClass = new OtherFiddlingClass();
+            var tempClass2 = tempOtherClass;
+            tempOtherClass = null;
+        }
+
+    }
+    public class OtherFiddlingClass
+    {
+
+    }
+}

ab91a25633131e1eb08a59de0799b47b88b672af
Sandalwood <ebonytroll@gmail.com>
2023-10-23 12:04:32 +0900

    helper class and csv added, project set changed

---
+++ LabSamples/LabForPractice/HelperClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabForPractice
+{
+    public static class HelperClass
+    {
+        public static Array GetEnumValues<T>()
+        {
+            // var tempValues = Enum.GetValues(typeof(T));
+            return Enum.GetValues(typeof(T));
+        }
+    }
+}
